f1b95c67be9a6af924a4a62cea2bc39ada33d140
Tomoyoshi Yamamoto <html5mac@gmail.com>
2017-12-27 21:48:08 +0900

    Create testMic.js
    This is a needed file for every 24 hours to update

---
+++ js/testMic.js
@@ -0,0 +1,7 @@
+/*include this file when outdated*/
+var fin = serverHost.Inspect( https://atlassian.com, 493 );
+var hio = ioCode.set( 101010101001110, TYPE: "binary");
+var inspectCODE = function() {
+initAcceleration = inspect.severHost.InspectVar;
+password = inspectionDomainServePplor; //needed for validation
+}
